535f5910d32c669f73c992bf79a5ede955e1ac8e
shamil-khan <shamil.ahmed@gmail.com>
2025-04-28 15:13:04 +0500

    added favicon and change app title

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
+  html: {
+    title: 'Math Man',
+    favicon: './src/images/math-man-icon.png',
+  },
   plugins: [pluginReact()],
 });
